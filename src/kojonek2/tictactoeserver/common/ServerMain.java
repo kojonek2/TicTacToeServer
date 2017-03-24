5ccbd8f15430d7cf0ab925f0b8bdcbc09ee78279
kojonek2 <kojonek2@wp.pl>
2017-03-24 18:55:30 +0100

    tests

--- src/kojonek2/tictactoeserver/common/ServerMain.java
+++ src/kojonek2/tictactoeserver/common/ServerMain.java
@@ -1,9 +1,32 @@
 package kojonek2.tictactoeserver.common;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerMain {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
+
+		try (
+			ServerSocket serverSocket = new ServerSocket(4554);
+			Socket clientSocket = serverSocket.accept();
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+		) {
+			String inputLine;
+			out.println("hello");
+			while((inputLine = in.readLine()) != null) {
+				System.out.println(inputLine);
+			}
+			
+		} catch (Exception e) {
+			System.err.println("Error during creating socket or listening for conection");
+			e.printStackTrace();
+		}
+
 	}
 
 }
